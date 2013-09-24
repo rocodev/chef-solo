9088a08fbb52507141cc4da7ae8f4ec07383d2ed
v1nc3ntlaw <v1nc3ntlaw@gmail.com>
2013-09-24 12:13:54 +0800

    Import nodejs version 1.3.0

--- cookbooks/nodejs/attributes/default.rb
+++ cookbooks/nodejs/attributes/default.rb
@@ -17,15 +17,22 @@
 # limitations under the License.
 #
 
-default['nodejs']['install_method'] = 'source'
-default['nodejs']['version'] = '0.10.2'
-default['nodejs']['checksum'] = '4eb642897fdb945b49720f2604afc493587aec7a9ff1537e882df659e4dd8aa2'
-default['nodejs']['checksum_linux_x64'] = '44ff658b1c3ae027b75310e0173b7d069ae70f6adaed23d22f2e087f5048c428'
-default['nodejs']['checksum_linux_x86'] = '9970b12b1dda8cbb6272d90b073da90336bce8667b2d57936106bd50c4a035dd'
+case node['platform_family']
+  when "smartos"
+    default['nodejs']['install_method'] = 'package'
+  else
+    default['nodejs']['install_method'] = 'source'
+end
+
+default['nodejs']['version'] = '0.10.15'
+default['nodejs']['checksum'] = '87345ab3b96aa02c5250d7b5ae1d80e620e8ae2a7f509f7fa18c4aaa340953e8'
+default['nodejs']['checksum_linux_x64'] = '0b5191748a91b1c49947fef6b143f3e5e5633c9381a31aaa467e7c80efafb6e9'
+default['nodejs']['checksum_linux_x86'] = '7ff9fb6aa19a5269a5a2f7a770040b8cd3c3b528a9c7c07da5da31c0d6dfde4d'
 default['nodejs']['dir'] = '/usr/local'
-default['nodejs']['npm'] = '1.2.14'
+default['nodejs']['npm'] = '1.3.5'
 default['nodejs']['src_url'] = "http://nodejs.org/dist"
 default['nodejs']['make_threads'] = node['cpu'] ? node['cpu']['total'].to_i : 2
+default['nodejs']['check_sha'] = true
 
 # Set this to true to install the legacy packages (0.8.x) from ubuntu/debian repositories; default is false (using the latest stable 0.10.x)
 default['nodejs']['legacy_packages'] = false

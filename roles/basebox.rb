945b8cc982430eff43136e4f60b1eb91bb0d7da7
v1nc3ntlaw <v1nc3ntlaw@gmail.com>
2013-10-03 11:06:58 +0800

    Use sudo cookbook configure server sudo settings

--- roles/basebox.rb
+++ roles/basebox.rb
@@ -1,10 +1,24 @@
-name "base-system"
+name "basebox"
 
 run_list [ "recipe[base]",
+           "recipe[sudo]",
            "recipe[users]",
            "recipe[rvm::system]" ]
 
 default_attributes(
+  # recipe[sudo]
+  :authorization => {
+    :sudo => {
+      :sudoers_defaults => [
+        'env_reset',
+        'exempt_group=admin',
+        'secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'],
+      :groups => [ "admin", "sudo" ],
+      :passwordless => false,
+      :include_sudoers_d => true
+    }
+  },
+  # recipe[rvm]
   :rvm => {
     :default_ruby => 'ruby-2.0.0-p247',
     :global_gems => [

f152717107eb39c041cd7fec92adff4ddd8d5ab1
v1nc3ntlaw <v1nc3ntlaw@gmail.com>
2013-10-03 14:28:43 +0800

    Update nginx attributes to recent version

--- roles/web-box.rb
+++ roles/web-box.rb
@@ -1,34 +1,38 @@
-name "webserver"
+name "web-box"
 
 run_list [ "recipe[memcached]",
            "recipe[nodejs]",
-           "recipe[nginx]",
-           "recipe[projects]" ]
+           "recipe[nginx]" ]
 
 default_attributes(
+  # recipe[memcached]
   :memcached => {
     :memory => 64
   },
+  # recipe[nodejs]
   :nodejs => {
     :install_method => "package"
   },
+  # recipe[nginx]
   :nginx => {
-    :version => "1.2.7",
+    :version => "1.4.2",
     :worker_processes => 4,
     :worker_connections => "8192",
     :worker_rlimit_nofile => "32768",
     :client_max_body_size => "34m",
     :event => "epoll",
+    :server_tokens => "off",
     :install_method => "source",
+    :configure_flags => [ "--with-http_spdy_module" ],
     :source => {
       :modules => [
-        "http_ssl_module", "http_gzip_static_module", "passenger"
+        "nginx::passenger"
       ]
     },
     :passenger => {
-      :version => "3.0.19",
-      :root => "/usr/local/rvm/gems/ruby-1.9.3-p392/gems/passenger-3.0.19",
-      :gem_binary => "/usr/local/rvm/rubies/ruby-1.9.3-p392/bin/gem",
+      :version => "4.0.19",
+      :root => "/usr/local/rvm/gems/ruby-2.0.0-p247/gems/passenger-4.0.19",
+      :gem_binary => "/usr/local/rvm/rubies/ruby-2.0.0-p247/bin/gem",
       :max_pool_size => "4"
     }
   },

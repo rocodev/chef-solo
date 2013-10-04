84d7866d63a2c8333983e44c924908e300f65ac1
v1nc3ntlaw <v1nc3ntlaw@gmail.com>
2013-10-04 11:58:33 +0800

    Add web-box role, percona mysql, redis to rocodev server

--- servers/rocodev.rb
+++ servers/rocodev.rb
@@ -4,4 +4,16 @@ port 22
 
 node.set[:name] = 'rocodev'
 
-run_list [ 'role[basebox]' ]
+run_list [
+  'role[basebox]',
+  'role[web-box]',
+  'recipe[percona::server]',
+  'recipe[redisio::install]',
+  'recipe[redisio::enable]'
+]
+
+# recipe[percona::server]
+node.set[:percona] = {
+  :main_config_file => "/etc/mysql/my.cnf",
+  :encrypted_data_bag => "rocodev"
+}

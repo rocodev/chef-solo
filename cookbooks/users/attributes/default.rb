04ea81d144c4544ae21f5ea9d9739049a43a4db9
v1nc3ntlaw <v1nc3ntlaw@gmail.com>
2013-10-04 11:52:01 +0800

    Add comment to users cookbook attributes file specify should use mkpasswd

--- cookbooks/users/attributes/default.rb
+++ cookbooks/users/attributes/default.rb
@@ -1,3 +1,5 @@
+# http://docs.opscode.com/resource_user.html#password-shadow-hash
+# Find a linux system using mkpasswd -m sha-512 to generate password hash
 
 default[:users][:apps][:auth_keys] = []
 default[:users][:apps][:password]  = "!"

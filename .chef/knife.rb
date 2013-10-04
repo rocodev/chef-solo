e9ed158156bdeab33c6b284b0517d93624ab68c0
v1nc3ntlaw <v1nc3ntlaw@gmail.com>
2013-10-04 11:23:11 +0800

    Add data bags path

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -9,6 +9,7 @@ cache_options :path => File.join(dir,     'solo/checksums')
 file_cache_path        File.join(dir,     'solo/files')
 file_backup_path       File.join(dir,     'solo/backup')
 cookbook_path        [ File.join(top_dir, 'cookbooks') ]
+data_bag_path        [ File.join(top_dir, 'data_bags') ]
 role_path            [ File.join(top_dir, 'roles') ]
 client_key             File.join(dir,     'insecure_client_key.pem')
 encrypted_data_bag_secret File.join(dir,  'encrypted_data_bag_secret')
